e2376c84f3ff9bb68ae532bcbeec0f81227e1bbd
Jared P. Lander <jared@landeranalytics.com>
2021-04-01 14:35:49 -0400

    Endpoints with parameters

--- code/plumber.r
+++ code/plumber.r
@@ -6,3 +6,20 @@ function()
 {
     return(list(name='Jared'))
 }
+
+#* Echo back the input
+#* @param msg Message to be printed
+#* @get /echo
+function(msg='default')
+{
+    list(msg=glue::glue("The message is '{msg}'."))
+}
+
+#* Add the two numbers
+#* @param x:numeric First number
+#* @param y:numeric Second number
+#* @get /add
+function(x, y)
+{
+    return(as.numeric(x) + as.numeric(y))
+}
