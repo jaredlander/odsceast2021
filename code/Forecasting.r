6cfd7b40bcb8a3717c40e66e6158182cae168339
Jared P. Lander <jared@landeranalytics.com>
2021-04-01 11:05:15 -0400

    Showed simple forecasting

--- code/Forecasting.r
+++ code/Forecasting.r
@@ -81,4 +81,54 @@ p <- elec %>%
     )
 plotly::toWebGL(p)
 
+# ACF ####
 
+# Autocorrelation function
+elec %>% ACF(ActivePower)
+elec %>% ACF(ActivePower) %>% autoplot()
+
+plot_acf_diagnostics(elec, .date_var=date, .value=ActivePower, .lags=40)
+
+# Simple Forecasting ####
+
+elec %>% autoplot(ActivePower)
+
+naive_mod <- elec %>% 
+    model(Naive=NAIVE(ActivePower))
+naive_mod
+naive_mod %>% select(Naive) %>% report()
+naive_mod %>% forecast(h=90)
+
+naive_mod %>% forecast(h=90) %>% autoplot()
+naive_mod %>% forecast(h=90) %>% autoplot(elec)
+naive_mod %>% forecast(h=90) %>% autoplot(elec %>% filter_index('2010' ~ .))
+
+naive_mod %>% forecast(h='90 days')
+naive_mod %>% forecast(h='3 months')                                          
+
+mean_mod <- elec %>% 
+    model(Mean=MEAN(ActivePower))
+mean_mod
+
+elec2010 <- elec %>% filter_index('2010' ~ .)
+mean_mod %>% forecast(h=90) %>% autoplot(elec2010)
+
+snaive_mod <- elec %>% 
+    model(SNaive=SNAIVE(ActivePower ~ lag('month') + lag('year') + lag('week')))
+snaive_mod %>% forecast(h=90) %>% autoplot(elec2010)          
+
+simple_mods <- elec %>% 
+    model(
+        Mean=MEAN(ActivePower),
+        Naive=NAIVE(ActivePower),
+        SNaive=SNAIVE(ActivePower ~ lag('month') + lag('year') + lag('week'))
+    )
+
+simple_mods
+simple_mods %>% select(SNaive) %>% report()
+simple_mods %>% glance()
+
+simple_mods %>% forecast(h=90)
+simple_mods %>% forecast(h=90) %>% View
+simple_mods %>% forecast(h=90) %>% autoplot(elec2010)
+simple_mods %>% forecast(h=90) %>% autoplot(elec2010, level=NULL)
