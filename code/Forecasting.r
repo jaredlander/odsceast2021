4ddf1fa9d9e0e92722fec80fe0f034a9091c84f2
Jared P. Lander <jared@landeranalytics.com>
2021-04-01 10:37:42 -0400

    Showed plotting

--- code/Forecasting.r
+++ code/Forecasting.r
@@ -23,3 +23,62 @@ elec <- elec %>%
     as_tsibble(index=Date) %>% 
     mutate(Year=lubridate::year(Date)) %>% 
     filter_index('2007' ~ .)
+
+# Time Series Formats ####
+
+ts_elec <- ts(elec$ActivePower, start=min(elec$Date), end=max(elec$Date))
+ts_elec
+ts_elec %>% class()
+ts_elec %>% plot()
+
+xts_elec <- xts::as.xts(ts_elec)
+xts_elec
+xts_elec %>% class()
+
+xts_elec %>% plot()
+
+# Visualization ####
+
+# from {feats}
+
+elec %>% autoplot(ActivePower)
+elec %>% gg_season(ActivePower, period='year')
+
+# from {timetk}
+
+elec %>% 
+    plot_time_series(
+        .date_var=Date, .value=ActivePower)
+elec %>% 
+    plot_time_series(
+        .date_var=Date, .value=ActivePower, 
+        .interactive=FALSE
+    )
+
+elec %>% 
+    plot_time_series(
+        .date_var=Date, .value=ActivePower, 
+        .color_var=Year,
+        .interactive=TRUE
+    )
+
+elec %>% 
+    plot_time_series(
+        .date_var=Date, .value=ActivePower, 
+        .color_var=Year,
+        .facet_vars=Year, .facet_scales='free_x',
+        .smooth=FALSE,
+        .interactive=TRUE
+    )
+
+p <- elec %>% 
+    plot_time_series(
+        .date_var=Date, .value=ActivePower, 
+        .color_var=Year,
+        .facet_vars=Year, .facet_scales='free_x',
+        .smooth=FALSE,
+        .interactive=TRUE
+    )
+plotly::toWebGL(p)
+
+
