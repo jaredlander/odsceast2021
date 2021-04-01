eef57cebf318cdc6c0a96f395b30a8539e025f86
Jared P. Lander <jared@landeranalytics.com>
2021-04-01 14:53:43 -0400

    Endpoint for prediction

--- code/plumber.r
+++ code/plumber.r
@@ -23,3 +23,29 @@ function(x, y)
 {
     return(as.numeric(x) + as.numeric(y))
 }
+
+the_mod <- readr::read_rds('mod0.rds')
+
+#* Predict status from individual data
+#* @param Income:numeric Income, expected in the hudreds
+#* @param Seniority:numeric Seniority, expected in the single digits
+#* @param Records:string "yes"/"no"
+#* @param Amount:numeric Amount in thousands
+#* @param Job:string Type of job, one of "fixed"/"freelance"/"others"/"partime"
+#* @get /predict
+function(Income, Seniority, Records, Amount, Job)
+{
+    the_data <- data.frame(
+        Income=as.numeric(Income),
+        Seniority=as.numeric(Seniority),
+        Records=Records,
+        Amount=as.numeric(Amount),
+        Job=Job
+    )
+    
+    return(
+        list(
+            predicted_status=predict(the_mod, new_data=the_data, type='prob')
+        )
+    )
+}
